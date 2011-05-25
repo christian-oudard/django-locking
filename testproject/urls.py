0bb0621363789f4cef187aabe4732b5b20a4ab5d
Christian Oudard <christian.oudard@gmail.com>
2011-05-25 17:47:12 -0400

    Added a test project that can run the unittests.

---
+++ testproject/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'testproject.views.home', name='home'),
+    # url(r'^testproject/', include('testproject.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^locking/', include('locking.urls')),
+)
